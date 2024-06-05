cbc82eee3eb574cda58cc8f1048389cbc9a26043
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-06-05 16:41:02 +0530

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int,int>mapping;
+            for(int i=0;i<nums.size();i++){
+                int rem=target-nums[i];
+                if(mapping.find(rem)!=mapping.end()) return {i,mapping[rem]};
+                else {
+                    mapping[nums[i]]=i;
+                
+                }
+            }
+            return {};
+    }
+};
